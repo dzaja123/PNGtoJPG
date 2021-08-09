8cbf5bc0da612f0ceae89db03888bfa04197c1ab
Nikola Džajević <44314786+dzaja123@users.noreply.github.com>
2021-08-09 09:43:28 +0200

    Add files via upload

---
+++ PNGtoJPG.py
@@ -0,0 +1,30 @@
+import os
+from PIL import Image
+
+read = "/home/dls/Pictures/folder5/" # folder sa .png slikama
+write = "/home/dls/Pictures/folder5/" # folder gde ce se cuvati .jpg slike
+
+names = os.listdir(read)
+brojac = 0 # brojac
+
+for name in names:
+
+    img = Image.open(read + name) # ucitavanje slika
+    name = name.split(".") # splitovanje po tacki
+    
+    if name[-1] == "png":
+        
+        name[-1] = "jpg" # definisanje formata
+        name = str.join(".", name) # konvertovanje u jpg, dodavanje sufiksa .jpg 
+        save_path = write + name # putanja za cuvanje 
+        
+        img = img.convert('RGB') # konvertovanje u RGB
+        img.save(save_path) # pilow funkcija za cuvanje
+        
+        brojac += 1 # inkrementovanje brojaca
+        print(save_path, "------brojac：", brojac) # ispis povratnih informaacija u konzoli
+    
+    else:
+ 
+        continue
+
